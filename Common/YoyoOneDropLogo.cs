6b232d7b0eab1fdb3419f52328b2524a3a3f44ee
spladison@gmail.com <spladison@gmail.com>
2024-01-24 01:20:12 +0300

    Remove OneDropLogo tooltip line from all yoyos

---
+++ Common/YoyoOneDropLogo.cs
@@ -0,0 +1,20 @@
+﻿using SPYoyoMod.Utils.Extensions;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod.Common
+{
+    public class YoyoOneDropLogo : GlobalItem
+    {
+        public override bool AppliesToEntity(Item item, bool lateInstantiation) { return item.IsYoyo(); }
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            var oneDropLogoTooltip = tooltips.Find(i => i.Name == "OneDropLogo");
+
+            if (oneDropLogoTooltip is not null)
+                tooltips.Remove(oneDropLogoTooltip);
+        }
+    }
+}
