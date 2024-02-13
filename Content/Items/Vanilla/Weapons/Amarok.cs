9a730a5d14d668c089093c697d416d284f9eec21
spladison@gmail.com <spladison@gmail.com>
2024-02-14 01:46:33 +0300

    Amarok development start

---
+++ Content/Items/Vanilla/Weapons/Amarok.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace SPYoyoMod.Content.Items.Vanilla.Weapons
+{
+    public class AmarokItem : VanillaYoyoItem
+    {
+        public override int YoyoType => ItemID.Amarok;
+    }
+
+    public class AmarokProjectile : ValorProjectile
+    {
+        public override int YoyoType => ProjectileID.Amarok;
+    }
+}
