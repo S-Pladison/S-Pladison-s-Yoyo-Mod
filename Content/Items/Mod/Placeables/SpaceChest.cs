d1c361270242bf85b9ee261b447b5a409e81cb92
spladison@gmail.com <spladison@gmail.com>
2024-02-17 01:33:53 +0300

    Added SpaceChest and TrappedSpaceChest

---
+++ Content/Items/Mod/Placeables/SpaceChest.cs
@@ -0,0 +1,48 @@
+﻿using SPYoyoMod.Content.Items.Mod.Miscellaneous;
+using Terraria;
+using Terraria.GameContent.Achievements;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod.Content.Items.Mod.Placeables
+{
+    public class SpaceChestItem : ModItem
+    {
+        public override string Texture => ModAssets.ItemsPath + "SpaceChest";
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<SpaceChestTile>());
+
+            Item.width = 32;
+            Item.height = 32;
+            Item.value = Terraria.Item.sellPrice(platinum: 0, gold: 0, silver: 5, copper: 0);
+        }
+    }
+
+    public class SpaceChestTile : ChestTile
+    {
+        public override string Texture => ModAssets.TilesPath + "SpaceChest";
+        public override int PlaceItemType => ModContent.ItemType<SpaceChestItem>();
+        public override int KeyItemType => ModContent.ItemType<SpaceKey>();
+
+        public override void ChestSetStaticDefaults()
+        {
+            DustType = ModContent.DustType<SpaceChestDust>();
+        }
+
+        public override bool UnlockChest(int i, int j, ref short frameXAdjustment, ref int dustType, ref bool manual)
+        {
+            if (!NPC.downedPlantBoss) return false;
+
+            AchievementsHelper.NotifyProgressionEvent(AchievementHelperID.Events.UnlockedBiomeChest);
+
+            return base.UnlockChest(i, j, ref frameXAdjustment, ref dustType, ref manual);
+        }
+    }
+
+    public class SpaceChestDust : ModDust
+    {
+        public override string Texture => ModAssets.DustsPath + "SpaceChest";
+    }
+}
---
+++ Content/Items/Mod/Placeables/TrappedSpaceChest.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod.Content.Items.Mod.Placeables
+{
+    public class TrappedSpaceChestItem : ModItem
+    {
+        public override string Texture => ModAssets.ItemsPath + "SpaceChest";
+
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.TrapSigned[Type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<TrappedSpaceChestTile>());
+
+            Item.width = 36;
+            Item.height = 36;
+            Item.value = Terraria.Item.sellPrice(platinum: 0, gold: 0, silver: 1, copper: 0);
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<SpaceChestItem>()
+                .AddIngredient(ItemID.Wire, 10)
+                .AddTile(TileID.HeavyWorkBench)
+                .Register();
+        }
+    }
+
+    public class TrappedSpaceChestTile : TrappedChestTile
+    {
+        public override string Texture => ModAssets.TilesPath + "TrappedSpaceChest";
+    }
+}
