e241649a28431d47bf3e8cc8e65607232e80491e
spladison@gmail.com <spladison@gmail.com>
2024-02-17 01:35:09 +0300

    Cleanup

--- Common/ModCompatibility/DialogueTweakCompatibility.cs
+++ Common/ModCompatibility/DialogueTweakCompatibility.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Xna.Framework;
 using System;
+using System.Collections.Generic;
 using Terraria.ModLoader;
-using Microsoft.Xna.Framework;
 
 namespace SPYoyoMod.Common.ModCompatibility
 {
