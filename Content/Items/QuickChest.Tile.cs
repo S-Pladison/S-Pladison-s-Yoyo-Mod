12064f183bcb2c028559accd2e90c0b52e3de175
spladison@gmail.com <spladison@gmail.com>
2024-02-25 16:01:49 +0300

    TriggerTrappedChestPacket moved to TrappedChestTile

--- Content/Items/QuickChest.Tile.cs
+++ Content/Items/QuickChest.Tile.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using SPYoyoMod.Common.Networking;
+using System.IO;
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
@@ -265,6 +266,41 @@ namespace SPYoyoMod.Content.Items
 
     public abstract class TrappedChestTile : ModTile
     {
+        private class TriggerTrappedChestPacket : NetPacket
+        {
+            public readonly short Left;
+            public readonly short Top;
+
+            public TriggerTrappedChestPacket() { }
+
+            public TriggerTrappedChestPacket(int left, int top)
+            {
+                Left = (short)left;
+                Top = (short)top;
+            }
+
+            public override void Send(BinaryWriter writer)
+            {
+                writer.Write(Left);
+                writer.Write(Top);
+            }
+
+            public override void Receive(BinaryReader reader, int sender)
+            {
+                var left = reader.ReadInt16();
+                var top = reader.ReadInt16();
+
+                Wiring.SetCurrentUser(sender);
+                TrappedChestTile.Trigger(left, top);
+                Wiring.SetCurrentUser(-1);
+
+                if (Main.netMode == NetmodeID.Server)
+                {
+                    new TriggerTrappedChestPacket(left, top).Send(-1, sender);
+                }
+            }
+        }
+
         public virtual Color MapColor { get => new(174, 129, 92); }
 
         public sealed override void SetStaticDefaults()
