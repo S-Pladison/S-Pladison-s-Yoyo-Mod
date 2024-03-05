9618d570519a1656673ab637fcbd185b71d58435
spladison@gmail.com <spladison@gmail.com>
2024-03-05 04:39:13 +0300

    Replace AdamantiteYoyo to ResidualLight

--- Content/Items/Mod/Weapons/ResidualLight.cs
+++ Content/Items/Mod/Weapons/ResidualLight.cs
@@ -4,34 +4,36 @@ using Terraria.ModLoader;
 
 namespace SPYoyoMod.Content.Items.Mod.Weapons
 {
-    public class AdamantiteYoyoItem : YoyoItem
+    public class ResidualLightItem : YoyoItem
     {
-        public override string Texture => ModAssets.ItemsPath + "AdamantiteYoyo";
+        public override string Texture => ModAssets.ItemsPath + "ResidualLight";
         public override int GamepadExtraRange => 15;
 
         public override void YoyoSetDefaults()
         {
-            Item.damage = 5;
+            Item.damage = 20;
             Item.knockBack = 2.5f;
 
-            Item.shoot = ModContent.ProjectileType<AdamantiteYoyoProjectile>();
+            Item.shoot = ModContent.ProjectileType<ResidualLightProjectile>();
 
-            Item.rare = ItemRarityID.LightRed;
+            Item.rare = ItemRarityID.Pink;
             Item.value = Terraria.Item.sellPrice(platinum: 0, gold: 1, silver: 50, copper: 0);
         }
 
         public override void AddRecipes()
         {
             CreateRecipe()
-                .AddIngredient(ItemID.AdamantiteBar, 12)
+                .AddIngredient(ItemID.Chik)
+                .AddIngredient(ItemID.HallowedBar, 12)
+                .AddIngredient(ItemID.LightShard, 2)
                 .AddTile(TileID.MythrilAnvil)
                 .Register();
         }
     }
 
-    public class AdamantiteYoyoProjectile : YoyoProjectile
+    public class ResidualLightProjectile : YoyoProjectile
     {
-        public override string Texture => ModAssets.ProjectilesPath + "AdamantiteYoyo";
+        public override string Texture => ModAssets.ProjectilesPath + "ResidualLight";
         public override float LifeTime => -1f;
         public override float MaxRange => 300f;
         public override float TopSpeed => 13f;
