2a11ede088340d4acec13b90db7e23b437ec0bd4
spladison@gmail.com <spladison@gmail.com>
2024-06-28 22:16:58 +0300

    Добавление класса для хранения общих событий

---
+++ ModEvents.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using SPYoyoMod.Utils;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod
+{
+    public class ModEvents : ILoadable
+    {
+        /// <summary>
+        /// Вызывается перед началом отрисовки игры.
+        /// </summary>
+        public static event Action OnPreDraw;
+
+        /// <summary>
+        /// Вызывается при изменении разрешения экрана.
+        /// </summary>
+        public static event Action<Vector2> OnResolutionChanged;
+
+        void ILoadable.Load(Mod mod)
+        {
+            LoadVanillaEvents();
+        }
+
+        void ILoadable.Unload()
+        {
+            UnloadVanillaEvents();
+        }
+
+        private static void LoadVanillaEvents()
+        {
+            OnPreDraw += ModUtils.EmptyAction;
+            Main.OnPreDraw += ModOnPreDraw;
+
+            OnResolutionChanged += ModUtils.EmptyAction;
+            Main.OnResolutionChanged += ModOnResolutionChanged;
+        }
+
+        private static void UnloadVanillaEvents()
+        {
+            Main.OnResolutionChanged -= ModOnResolutionChanged;
+            OnResolutionChanged = null;
+
+            Main.OnPreDraw -= ModOnPreDraw;
+            OnPreDraw = null;
+        }
+
+        private static void ModOnPreDraw(GameTime _)
+            => ModEvents.OnPreDraw();
+
+        private static void ModOnResolutionChanged(Vector2 screenSize)
+            => ModEvents.OnResolutionChanged(screenSize);
+    }
+}
---
+++ Utils/ModUtils.cs
@@ -0,0 +1,9 @@
+﻿namespace SPYoyoMod.Utils
+{
+    public static class ModUtils
+    {
+        public static void EmptyAction() { }
+        public static void EmptyAction<T>(T _) { }
+        public static void EmptyAction<T1, T2>(T1 _1, T2 _2) { }
+    }
+}
