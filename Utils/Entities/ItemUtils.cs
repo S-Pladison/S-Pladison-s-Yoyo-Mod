5dc9056af015a6c1e8f5009398050dc233f70674
spladison@gmail.com <spladison@gmail.com>
2024-06-30 22:35:22 +0300

    Добавление функций проверки снарядов и предметов на то, являются ли они йо-йо

--- Utils/Entities/ItemUtils.cs
+++ Utils/Entities/ItemUtils.cs
@@ -1,10 +1,30 @@
 ﻿using System.Runtime.CompilerServices;
 using Terraria;
+using Terraria.ID;
 
 namespace SPYoyoMod.Utils
 {
     public static class ItemUtils
     {
+        /// <summary>
+        /// Является ли этот предмет оружием типа йо-йо.
+        /// </summary>
+        public static bool IsYoyo(this Item item)
+        {
+            if (ItemID.Sets.Yoyo[item.type]) return true;
+            if (item.shoot <= ProjectileID.None) return false;
+
+            var dict = ContentSamples.ProjectilesByType;
+
+            if (dict.TryGetValue(item.shoot, out Projectile proj))
+                return proj.IsYoyo();
+
+            proj = new Projectile();
+            proj.SetDefaults(item.shoot);
+
+            return proj.IsYoyo();
+        }
+
         /// <summary>
         /// Конвертирует предоставленную стоимость продажи в медные монеты. Это значение в пять раз превышает <see cref="Item.buyPrice"/>.<br/>
         /// Если присвоено <see cref="Item.value"/>, то предмет будет продан за указанную стоимость.
---
+++ Utils/Entities/ProjectileUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using Terraria;
+using Terraria.ID;
+
+namespace SPYoyoMod.Utils
+{
+    public static class ProjectileUtils
+    {
+        /// <summary>
+        /// Является ли этот снаряд снарядом от йо-йо. Снаряды противовесов также относятся к йо-йо.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsYoyo(this Projectile proj)
+        {
+            return proj.aiStyle.Equals(ProjAIStyleID.Yoyo);
+        }
+    }
+}
