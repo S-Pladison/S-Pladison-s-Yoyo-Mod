18ce2cf9b25b460eb1ce3f8767710730b2520b61
spladison@gmail.com <spladison@gmail.com>
2024-06-30 22:36:10 +0300

    Удаление строки 'OneDropLogo' со всех йо-йо

---
+++ Common/YoyoOneDropLogo.cs
@@ -0,0 +1,16 @@
+﻿using SPYoyoMod.Utils;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod.Common
+{
+    public sealed class YoyoOneDropLogoGlobalItem : GlobalItem
+    {
+        public override bool AppliesToEntity(Item item, bool lateInstantiation)
+            => lateInstantiation && item.IsYoyo();
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+            => tooltips.Find(i => i.Mod == "Terraria" && i.Name == "OneDropLogo")?.Hide();
+    }
+}
