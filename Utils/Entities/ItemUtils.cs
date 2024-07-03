73e8da1f3e5758ee08138fdb42856ba218c1edf7
spladison@gmail.com <spladison@gmail.com>
2024-07-04 02:22:48 +0300

    Добавлены утилиты для работы со всплывающими подсказками предметов

--- Utils/Entities/ItemUtils.cs
+++ Utils/Entities/ItemUtils.cs
@@ -1,6 +1,9 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Terraria;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace SPYoyoMod.Utils
 {
@@ -33,5 +36,67 @@ namespace SPYoyoMod.Utils
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int SellPrice(int platinum = 0, int gold = 0, int silver = 0, int copper = 0)
             => Item.sellPrice(platinum, gold, silver, copper);
+
+        /// <summary>
+        /// Разбивает строку на массив подстрок. Принимает массив символов, которые и будут служить разделителями.
+        /// </summary>
+        public static TooltipLine[] Split(this TooltipLine line, params char[] separator)
+        {
+            if (!ModLoader.TryGetMod(line.Mod, out var mod))
+                throw new Exception($"Failed to find mod {line.Mod}...");
+
+            var split = line.Text.Split(separator);
+            var lines = new TooltipLine[split.Length];
+
+            for (var i = 0; i < split.Length; i++)
+            {
+                lines[i] = new(mod, line.Name + i.ToString(), split[i])
+                {
+                    IsModifier = line.IsModifier,
+                    IsModifierBad = line.IsModifierBad,
+                    OverrideColor = line.OverrideColor
+                };
+            }
+
+            return lines;
+        }
+
+        /// <summary>
+        /// Вставляет строку в позицию, где обычно находится описание предмета.
+        /// </summary>
+        public static void InsertDescription(this List<TooltipLine> tooltips, TooltipLine line)
+            => tooltips.InsertDescription([line]);
+
+        /// <summary>
+        /// Вставляет строки в позицию, где обычно находится описание предмета.
+        /// </summary>
+        public static void InsertDescription(this List<TooltipLine> tooltips, IList<TooltipLine> lines)
+        {
+            for (var i = tooltips.Count - 1; i >= 0; i--)
+            {
+                var tooltipLine = tooltips[i];
+
+                if (tooltipLine.Mod != "Terraria")
+                    continue;
+
+                if (!tooltipLine.Name.StartsWith("Tooltip") && !_descriptionWhitelistSet.Contains(tooltipLine.Name))
+                    continue;
+
+                for (var j = 0; j < lines.Count; j++)
+                    tooltips.Insert(i + j + 1, lines[j]);
+
+                return;
+            }
+        }
+        
+        private static readonly HashSet<string> _descriptionWhitelistSet =
+        [
+            "Material", "Consumable", "Ammo", "Placeable", "UseMana", "HealMana",
+            "HealLife", "TileBoost", "HammerPower", "AxePower", "PickPower", "Defense",
+            "Vanity", "Quest", "WandConsumes", "Equipable", "BaitPower", "NeedsBait",
+            "FishingPower", "Knockback", "SpecialSpeedScaling", "NoSpeedScaling",
+            "Speed", "CritChance", "Damage", "SocialDesc", "Social", "NoTransfer",
+            "FavoriteDesc", "Favorite", "ItemName"
+        ];
     }
 }
