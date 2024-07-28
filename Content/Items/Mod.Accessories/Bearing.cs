99814ec0d81a1efd98b1ba50eaa21ebf35cbab2b
spladison@gmail.com <spladison@gmail.com>
2024-07-28 05:15:59 +0300

    Добавлен эффект увеличения времени жизни йо-йо

--- Content/Items/Mod.Accessories/Bearing.cs
+++ Content/Items/Mod.Accessories/Bearing.cs
@@ -1,13 +1,18 @@
+using SPYoyoMod.Common.Hooks;
 using SPYoyoMod.Utils;
 using Terraria;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace SPYoyoMod.Content.Items.Mod.Accessories
 {
     public sealed class BearingItem : ModItem
     {
+        public const int StatBonusInPercentage = 50;
+
         public override string Texture => $"{nameof(SPYoyoMod)}/Assets/Items/Mod.Accessories/Bearing_Item";
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(StatBonusInPercentage);
 
         public override void SetDefaults()
         {
@@ -37,6 +42,20 @@ namespace SPYoyoMod.Content.Items.Mod.Accessories
         public void Equip()
         {
             Equipped = true;
+        }
+    }
+
+    public sealed class BearingGlobalProjectile : GlobalProjectile, IModifyYoyoStatsProjectile
+    {
+        public override bool AppliesToEntity(Projectile proj, bool lateInstantiation)
+            => lateInstantiation && proj.IsYoyo() && !proj.IsCounterweight();
+
+        public void ModifyYoyoStats(Projectile proj, ref YoyoStatModifiers statModifiers)
+        {
+            if (!proj.GetOwner().GetModPlayer<BearingPlayer>().Equipped)
+                return;
+
+            statModifiers.LifeTime += BearingItem.StatBonusInPercentage / 100.0f;
         }
     }
 }
