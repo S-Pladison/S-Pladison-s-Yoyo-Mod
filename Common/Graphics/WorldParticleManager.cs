fe4ac0c6b9af172767eba46e0b8ddc36d991fd73
spladison@gmail.com <spladison@gmail.com>
2024-08-01 05:52:40 +0300

    Исправление отрисовки частиц

--- Common/Graphics/WorldParticleManager.cs
+++ Common/Graphics/WorldParticleManager.cs
@@ -3,7 +3,8 @@ using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Threading;
 using SPYoyoMod.Common.Graphics.RenderTargets;
 using SPYoyoMod.Utils;
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Terraria;
 using Terraria.ModLoader;
@@ -25,15 +26,11 @@ namespace SPYoyoMod.Common.Graphics
         public const int MaxParticles = 1000;
 
         private static Mutex _mutex = new();
-        private static FastList<IWorldParticle> _particles = new(MaxParticles);
-        private static FastList<IWorldParticle> _tempParticles = new(MaxParticles);
+        private static List<IWorldParticle> _particles = new(MaxParticles);
         private static ScreenRenderTarget _pixelatedTarget = ScreenRenderTarget.Create(ScreenRenderTargetScale.TwiceSmaller);
         private static bool _pixelatedTargetWasPrepared;
 
-        public static int ParticleCount
-        {
-            get => _particles.Length;
-        }
+        public static int ParticleCount { get => _particles.Count; }
 
         public static T SpawnParticle<T>() where T : IWorldParticle, new()
             => SpawnParticle(new T());
@@ -46,8 +43,7 @@ namespace SPYoyoMod.Common.Graphics
             if (ParticleCount >= MaxParticles)
                 return particle;
 
-            _particles.Buffer[_particles.Length] = particle;
-            _particles.Length++;
+            _particles.Add(particle);
 
             return particle;
         }
@@ -59,8 +55,11 @@ namespace SPYoyoMod.Common.Graphics
 
             On_Main.DrawDust += (orig, main) =>
             {
-                DrawDefaultParticles();
-                DrawPixelatedParticles();
+                if (ParticleCount > 0)
+                {
+                    DrawDefaultParticles();
+                    DrawPixelatedParticles();
+                }
 
                 orig(main);
             };
@@ -71,30 +70,28 @@ namespace SPYoyoMod.Common.Graphics
             ModEvents.OnPreUpdateDusts -= UpdateParticles;
 
             _pixelatedTarget = null;
-            _tempParticles = null;
             _particles = null;
             _mutex = null;
         }
 
         private static void UpdateParticles()
         {
-            if (ParticleCount <= 0)
+            if (ParticleCount <= 0 || Main.gameInactive)
                 return;
 
-            var shouldBeRemovedParticles = _tempParticles;
+            var shouldBeRemovedParticles = new List<IWorldParticle>(MaxParticles / 10);
 
             FastParallel.For(0, ParticleCount, (from, to, context) =>
             {
                 for (int i = from; i < to; i++)
                 {
-                    ref var particle = ref _particles.Buffer[i];
+                    var particle = _particles[i];
 
                     if (particle.ShouldBeRemoved)
                     {
                         _mutex.WaitOne();
 
-                        shouldBeRemovedParticles.Buffer[shouldBeRemovedParticles.Length] = particle;
-                        shouldBeRemovedParticles.Length++;
+                        shouldBeRemovedParticles.Add(particle);
 
                         _mutex.ReleaseMutex();
                     }
@@ -105,53 +102,36 @@ namespace SPYoyoMod.Common.Graphics
                 }
             });
 
-            for (int i = 0; i < shouldBeRemovedParticles.Length; i++)
+            for (int i = 0; i < shouldBeRemovedParticles.Count; i++)
                 _particles.RemoveAt(i);
 
             shouldBeRemovedParticles.Clear();
         }
 
-        private static ref FastList<IWorldParticle> GetParticlesByCondition(Predicate<IWorldParticle> predicate)
-        {
-            _tempParticles.Clear();
-
-            for (var i = 0; i < ParticleCount; i++)
-            {
-                ref var particle = ref _particles.Buffer[i];
-
-                if (predicate(particle))
-                {
-                    _tempParticles.Buffer[_tempParticles.Length] = particle;
-                    _tempParticles.Length++;
-                }
-            }
-
-            return ref _tempParticles;
-        }
-
         private static void DrawDefaultParticles()
         {
-            ref var defaultParticles = ref GetParticlesByCondition(p => !p.IsPixelated);
+            var defaultParticles = _particles.Where(p => !p.IsPixelated);
 
-            if (defaultParticles.Length <= 0)
+            if (!defaultParticles.Any())
                 return;
 
-            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, GameMatrices.Transform);
+            Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, Main.Rasterizer, null, GameMatrices.Transform);
 
-            for (var i = 0; i < defaultParticles.Length; i++)
-            {
-                defaultParticles.Buffer[i].Draw(Main.screenPosition);
-            }
+            foreach (var particle in defaultParticles)
+                particle.Draw(Main.screenPosition);
 
             Main.spriteBatch.End();
         }
 
         private static void RenderPixelatedParticles()
         {
-            var device = Main.graphics.GraphicsDevice;
-            ref var pixelatedParticles = ref GetParticlesByCondition(p => p.IsPixelated);
+            if (ParticleCount < 0)
+                return;
 
-            if (pixelatedParticles.Length <= 0)
+            var device = Main.graphics.GraphicsDevice;
+            var pixelatedParticles = _particles.Where(p => p.IsPixelated);
+
+            if (!pixelatedParticles.Any())
                 return;
 
             _pixelatedTargetWasPrepared = false;
@@ -159,12 +139,10 @@ namespace SPYoyoMod.Common.Graphics
             device.SetRenderTarget(_pixelatedTarget);
             device.Clear(Color.Transparent);
             {
-                Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, GameMatrices.Effect * Matrix.CreateScale(0.5f));
+                Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, Main.Rasterizer, null, GameMatrices.Effect * Matrix.CreateScale(0.5f));
 
-                for (var i = 0; i < pixelatedParticles.Length; i++)
-                {
-                    pixelatedParticles.Buffer[i].Draw(Main.screenPosition);
-                }
+                foreach (var particle in pixelatedParticles)
+                    particle.Draw(Main.screenPosition);
 
                 Main.spriteBatch.End();
             }
