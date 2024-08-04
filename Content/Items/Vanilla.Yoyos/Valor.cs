dd52425d59db15166a7a912939e10b3c083409d2
spladison@gmail.com <spladison@gmail.com>
2024-08-04 05:59:34 +0300

    Проработка логики закрепления врага цепью к плитке; Обновление файлов локализации

--- Content/Items/Vanilla.Yoyos/Valor.cs
+++ Content/Items/Vanilla.Yoyos/Valor.cs
@@ -1,3 +1,6 @@
+using Microsoft.Xna.Framework;
+using SPYoyoMod.Utils;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -21,5 +24,36 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
     public sealed class ValorProjectile : VanillaYoyoBaseProjectile
     {
         public override int ProjType => ProjectileID.Valor;
+
+        public override void OnHitNPC(Projectile proj, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (!target.TryGetGlobalNPC<ValorGlobalNPC>(out var globalNPC))
+                return;
+
+            globalNPC.ChainToTile(target);
+        }
+    }
+
+    public sealed class ValorGlobalNPC : GlobalNPC
+    {
+        public override bool InstancePerEntity => true;
+
+        public void ChainToTile(NPC npc)
+        {
+            if (!npc.CanBeChasedBy() || npc.IsBossOrRelated())
+                return;
+
+            if (!TryFindSuitableTile(npc, out var tileCoord))
+                return;
+
+            // ...
+        }
+
+        private bool TryFindSuitableTile(NPC npc, out Point tileCoord)
+        {
+            tileCoord = default;
+
+            return true;
+        }
     }
 }
