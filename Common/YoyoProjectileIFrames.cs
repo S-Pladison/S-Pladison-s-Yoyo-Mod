53cb3c7e98ccd4ddbddf7d72e61aa5102acd21e2
spladison@gmail.com <spladison@gmail.com>
2024-08-04 12:28:33 +0300

    Добавлен IFrames для всех йо-йо

---
+++ Common/YoyoProjectileIFrames.cs
@@ -0,0 +1,31 @@
+﻿using SPYoyoMod.Utils;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod.Common
+{
+    public sealed class YoyoProjectileIFramesGlobalProjectile : GlobalProjectile
+    {
+        public const int DefaulLocalIFrameValue = 10;
+
+        public override bool AppliesToEntity(Projectile proj, bool lateInstantiation)
+            => lateInstantiation && proj.IsYoyo() && !proj.IsCounterweight();
+
+        public override void SetDefaults(Projectile proj)
+        {
+            if (proj.usesLocalNPCImmunity || proj.usesIDStaticNPCImmunity)
+                return;
+
+            ApplyDefaultYoyoLocalIFrame(proj);
+        }
+
+        private static void ApplyDefaultYoyoLocalIFrame(Projectile proj)
+        {
+            proj.usesLocalNPCImmunity = true;
+            proj.localNPCHitCooldown = DefaulLocalIFrameValue * proj.MaxUpdates;
+
+            proj.usesIDStaticNPCImmunity = false;
+            proj.idStaticNPCHitCooldown = 0;
+        }
+    }
+}
