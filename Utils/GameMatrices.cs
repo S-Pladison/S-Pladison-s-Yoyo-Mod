d872c9b7233c6d2942b94befd9dec1cceb3a9869
spladison@gmail.com <spladison@gmail.com>
2024-08-09 05:35:28 +0300

    Удален ненужный комментарий

--- Utils/GameMatrices.cs
+++ Utils/GameMatrices.cs
@@ -7,12 +7,6 @@ namespace SPYoyoMod.Utils
 {
     public static class GameMatrices
     {
-        // TODO: Возможный фикс при тряске во время изменения зума...
-        // Проверь при создании йо-йо Доблесть
-        // float val = (float)screenWidth / (ModLoader.removeForcedMinimumZoom ? 8192f : MinimumZoomComparerX);
-        // float val2 = (float)screenHeight / (ModLoader.removeForcedMinimumZoom ? 8192f : MinimumZoomComparerY);
-        // ForcedMinimumZoom = Math.Max(Math.Max(1f, val), val2);
-        // Zoom = new Vector2(ForcedMinimumZoom * MathHelper.Clamp(GameZoomTarget, 1f, 2f));
         public static Matrix Zoom { get => Main.GameViewMatrix.ZoomMatrix; }
         public static Matrix Effect { get => GameMatricesHandler.Effect; }
         public static Matrix Transform { get => Main.GameViewMatrix.TransformationMatrix; }
