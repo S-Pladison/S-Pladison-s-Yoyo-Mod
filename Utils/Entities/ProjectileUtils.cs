7baab174a2715117c91a3476524a5cbfa7051383
spladison@gmail.com <spladison@gmail.com>
2024-11-02 05:36:41 +0300

    Исправление комментария

--- Utils/Entities/ProjectileUtils.cs
+++ Utils/Entities/ProjectileUtils.cs
@@ -11,7 +11,7 @@ namespace SPYoyoMod.Utils
     public static class ProjectileUtils
     {
         /// <summary>
-        /// Является ли этот снаряд снарядом от йо-йо. Снаряды противовесов также относятся к йо-йо.
+        /// Является ли этот снаряд йо-йом.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsYoyo(this Projectile proj)
