7a3ab6f9627d4c52886a512e0e9060e099418555
spladison@gmail.com <spladison@gmail.com>
2024-11-07 22:21:46 +0300

    Начало реворка йо-йо Code1

---
+++ Content/Items/Vanilla.Yoyos/Code1.cs
@@ -0,0 +1,28 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
+{
+    public sealed class Code1Assets : ILoadable
+    {
+        private const string _assetPath = $"{nameof(SPYoyoMod)}/Assets/";
+        private const string _yoyoPath = $"{_assetPath}Items/Vanilla.Yoyos/Code1/";
+
+        void ILoadable.Unload()
+        {
+            // ...
+        }
+
+        void ILoadable.Load(Terraria.ModLoader.Mod mod) { }
+    }
+
+    public sealed class Code1Item : VanillaYoyoBaseItem
+    {
+        public override int ItemType => ItemID.Code1;
+    }
+
+    public sealed class Code1Projectile : VanillaYoyoBaseProjectile
+    {
+        public override int ProjType => ProjectileID.Code1;
+    }
+}
