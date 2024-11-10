c5f9ba83c37b91b987e1f25fd7895a8dcc722256
spladison@gmail.com <spladison@gmail.com>
2024-11-10 18:03:04 +0300

    Начало разработки йо-йо TheStellarThrow

---
+++ Content/Items/Mod.Yoyos/TheStellarThrow.cs
@@ -0,0 +1,50 @@
+﻿using SPYoyoMod.Utils;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod.Content.Items.Mod.Yoyos
+{
+    [Autoload(Side = ModSide.Client)]
+    public sealed class TheStellarThrowAssets : ILoadable
+    {
+        public const string ItemPath = $"{_yoyoPath}TheStellarThrow_Item";
+        public const string ProjPath = $"{_yoyoPath}TheStellarThrow_Proj";
+
+        private const string _assetPath = $"{nameof(SPYoyoMod)}/Assets/";
+        private const string _yoyoPath = $"{_assetPath}Items/Mod.Yoyos/TheStellarThrow/";
+
+        void ILoadable.Unload()
+        {
+            // ...
+        }
+
+        void ILoadable.Load(Terraria.ModLoader.Mod mod) { }
+    }
+
+    public sealed class TheStellarThrowItem : YoyoBaseItem
+    {
+        public override string Texture => TheStellarThrowAssets.ItemPath;
+        public override int GamepadExtraRange => 10;
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+
+            Item.damage = 18;
+            Item.knockBack = 3f;
+
+            Item.shoot = ModContent.ProjectileType<TheStellarThrowProjectile>();
+
+            Item.rare = ItemRarityID.Green;
+            Item.value = ItemUtils.SellPrice(platinum: 0, gold: 1, silver: 0, copper: 0);
+        }
+    }
+
+    public sealed class TheStellarThrowProjectile : YoyoBaseProjectile
+    {
+        public override string Texture => TheStellarThrowAssets.ProjPath;
+        public override float LifeTime => -1f;
+        public override float MaxRange => 235f;
+        public override float TopSpeed => 14f;
+    }
+}
