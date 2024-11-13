f7525554bee35cc5f45f430ab08b7ebd454dfad4
spladison@gmail.com <spladison@gmail.com>
2024-11-14 01:45:31 +0300

    Всем йо-йо из ThoriumMod в ItemID.Sets.Yoyo[type] теперь присваивается значение true

--- Core/ModSupport/ThoriumMod.cs
+++ Core/ModSupport/ThoriumMod.cs
@@ -1,8 +1,23 @@
-﻿namespace SPYoyoMod.Core.ModSupport
+﻿using SPYoyoMod.Utils;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod.Core.ModSupport
 {
-    [ModInternalName("ThoriumMod")]
     public sealed class ThoriumModSupport : ModSupportSystem<ThoriumModSupport>
     {
         private ThoriumModSupport() { }
+
+        public override void PostSetupContent()
+        {
+            // Делаем так, чтобы их йо-йо действительно были йо-йо в ванильном понимании...
+            foreach (var modItem in Data.Instance.GetContent<ModItem>())
+            {
+                var itemType = modItem.Type;
+
+                if (ContentSamples.ItemsByType[itemType].IsYoyo())
+                    ItemID.Sets.Yoyo[itemType] = true;
+            }
+        }
     }
 }
