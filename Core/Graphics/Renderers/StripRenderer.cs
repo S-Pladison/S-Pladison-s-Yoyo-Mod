881cccaa50105821287e94d597e7dc888a553634
spladison@gmail.com <spladison@gmail.com>
2024-11-14 03:05:38 +0300

    Перенос Vertex2DPositionColorTexture.cs в другую папку

--- Core/Graphics/Renderers/StripRenderer.cs
+++ Core/Graphics/Renderers/StripRenderer.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using SPYoyoMod.Utils;
+using SPYoyoMod.Utils.DataStructures;
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
