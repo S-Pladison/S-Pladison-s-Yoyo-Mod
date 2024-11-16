e9aa082d519ba8903cb450de630a88ba26cb0ec4
spladison@gmail.com <spladison@gmail.com>
2024-11-16 04:04:25 +0300

    Флаг ItemID.Sets.Yoyo[...] теперь устанавливается для всех йо-йо, а не только из Thorium-а

---
+++ Common/Yoyos/Item.StaticYoyoFlag.cs
@@ -0,0 +1,24 @@
+﻿using SPYoyoMod.Utils;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod.Common.Yoyos
+{
+    public sealed class StaticYoyoFlagGlobalItem : GlobalItem
+    {
+        public override void Load()
+        {
+            // Делаем так, чтобы йо-йо из других модов действительно считались как йо-йо в ванильном понимании...
+            ModEvents.OnPostSetupContent += static () =>
+            {
+                foreach (var (type, item) in ContentSamples.ItemsByType)
+                {
+                    if (item.IsYoyo())
+                    {
+                        ItemID.Sets.Yoyo[type] = true;
+                    }
+                }
+            };
+        }
+    }
+}
--- Core/ModSupport/ThoriumSupport.cs
+++ Core/ModSupport/ThoriumSupport.cs
@@ -1,23 +1,7 @@
-﻿using SPYoyoMod.Utils;
-using Terraria.ID;
-using Terraria.ModLoader;
-
-namespace SPYoyoMod.Core.ModSupport
+﻿namespace SPYoyoMod.Core.ModSupport
 {
     public sealed class ThoriumSupport : ModSupportSystem<ThoriumSupport>
     {
         private ThoriumSupport() : base(internalName: "ThoriumMod") { }
-
-        public override void PostSetupContent()
-        {
-            // Делаем так, чтобы их йо-йо действительно были йо-йо в ванильном понимании...
-            foreach (var modItem in Instance.GetContent<ModItem>())
-            {
-                var itemType = modItem.Type;
-
-                if (ContentSamples.ItemsByType[itemType].IsYoyo())
-                    ItemID.Sets.Yoyo[itemType] = true;
-            }
-        }
     }
 }
