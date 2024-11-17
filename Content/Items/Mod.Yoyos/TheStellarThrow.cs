479f47a9636758344122a5471de62afb983c6bdc
spladison@gmail.com <spladison@gmail.com>
2024-11-17 05:21:35 +0300

    Добавлены обводка и трейл

--- Content/Items/Mod.Yoyos/TheStellarThrow.cs
+++ Content/Items/Mod.Yoyos/TheStellarThrow.cs
@@ -1,4 +1,12 @@
-﻿using SPYoyoMod.Utils;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using SPYoyoMod.Content.Items.Vanilla.Yoyos;
+using SPYoyoMod.Core.Graphics.Renderers;
+using SPYoyoMod.Core.Hooks;
+using SPYoyoMod.Utils;
+using System.Collections.Generic;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -10,12 +18,20 @@ namespace SPYoyoMod.Content.Items.Mod.Yoyos
         public const string ItemPath = $"{_yoyoPath}TheStellarThrow_Item";
         public const string ProjPath = $"{_yoyoPath}TheStellarThrow_Proj";
 
+        public static Asset<Texture2D> GlowTexture { get; private set; } = ModContent.Request<Texture2D>($"{_assetPath}YoyoGlow_WithShadow");
+        public static Asset<Texture2D> StarTexture { get; private set; } = ModContent.Request<Texture2D>($"{_yoyoPath}TheStellarThrow_Star");
+        public static Asset<Texture2D> FlameTexture { get; private set; } = ModContent.Request<Texture2D>($"{_yoyoPath}TheStellarThrow_Flame");
+        public static Asset<Effect> TrailEffect { get; private set; } = ModContent.Request<Effect>($"{_yoyoPath}TheStellarThrow_Trail");
+
         private const string _assetPath = $"{nameof(SPYoyoMod)}/Assets/";
         private const string _yoyoPath = $"{_assetPath}Items/Mod.Yoyos/TheStellarThrow/";
 
         void ILoadable.Unload()
         {
-            // ...
+            GlowTexture = null;
+            StarTexture = null;
+            FlameTexture = null;
+            TrailEffect = null;
         }
 
         void ILoadable.Load(Terraria.ModLoader.Mod mod) { }
@@ -40,11 +56,127 @@ namespace SPYoyoMod.Content.Items.Mod.Yoyos
         }
     }
 
-    public sealed class TheStellarThrowProjectile : YoyoBaseProjectile
+    public sealed class TheStellarThrowProjectile : YoyoBaseProjectile, IInitializableProjectile, IPreDrawPixelatedProjectile
     {
+        public static readonly Color GlowColor = new(252, 194, 116);
+        public static readonly Color StarColor = new(255, 0, 80);
+        public static readonly int TrailPointCount = 15;
+
+        private YoyoStringRenderer _stringRenderer;
+        private StripRenderer _trailRenderer;
+        private LinkedList<Vector2> _oldPositions;
+
         public override string Texture => TheStellarThrowAssets.ProjPath;
         public override float LifeTime => -1f;
         public override float MaxRange => 235f;
         public override float TopSpeed => 14f;
+
+        void IInitializableProjectile.Initialize(Projectile proj)
+        {
+            if (Main.netMode == NetmodeID.Server)
+                return;
+
+            _stringRenderer = new YoyoStringRenderer(new IDrawYoyoStringSegments.Gradient(
+               ModContent.Request<Texture2D>(CascadeAssets.StringPath, AssetRequestMode.ImmediateLoad).Value,
+               (Color.Transparent, true), (Color.Transparent, true), (GlowColor, true)
+            ));
+
+            _trailRenderer = new StripRenderer(Main.graphics.GraphicsDevice, capacity: TrailPointCount)
+            {
+                StartWidth = 45,
+                EndWidth = 25
+            };
+
+            _oldPositions = [];
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            _trailRenderer?.Dispose();
+        }
+
+        public override void AI()
+        {
+            UpdateVisual();
+        }
+
+        private void UpdateVisual()
+        {
+            if (_trailRenderer is not null)
+            {
+                _oldPositions.AddFirst(Projectile.Center + Projectile.velocity);
+
+                while (_oldPositions.Count > TrailPointCount)
+                    _oldPositions.RemoveLast();
+
+                _trailRenderer.SetPoints(_oldPositions);
+            }
+
+            Projectile.rotation -= 0.15f;
+
+            Lighting.AddLight(Projectile.Center, StarColor.ToVector3() * 0.2f);
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+            => Color.White;
+
+        void IPreDrawPixelatedProjectile.PreDrawPixelated(Projectile proj)
+        {
+            if (_trailRenderer is not null)
+            {
+                TheStellarThrowAssets.TrailEffect
+                    .Prepare(parameters =>
+                    {
+                        parameters["Texture0"].SetValue(TheStellarThrowAssets.FlameTexture.Value);
+                        parameters["TransformMatrix"].SetValue(GameMatrices.World * GameMatrices.Effect * GameMatrices.Projection);
+                        parameters["Color0"].SetValue(new Color(255, 50, 160).ToVector4());
+                        parameters["Color1"].SetValue(new Color(170, 30, 90).ToVector4());
+                        parameters["Color2"].SetValue(new Color(50, 50, 255).ToVector4());
+                        parameters["Color3"].SetValue(new Color(60, 55, 90).ToVector4());
+                        parameters["Repeats"].SetValue(_trailRenderer.Points.Distance() / CascadeAssets.FlameTexture.Width() / 128.0f / 4.0f);
+                        parameters["Time"].SetValue(Main.GlobalTimeWrappedHourly);
+                    })
+                    .Apply();
+
+                _trailRenderer.Render();
+            }
+
+            var starTexture = TheStellarThrowAssets.StarTexture;
+            var starPosition = Projectile.Center + Projectile.gfxOffY * Vector2.UnitY - Main.screenPosition;
+            var starOrigin = starTexture.Size() * 0.5f;
+            var starColor = new Color(100, 25, 75) * 0.35f;
+
+            Main.spriteBatch.Draw(starTexture.Value, starPosition, null, starColor, Projectile.rotation * 0.05f, starOrigin, proj.scale * 0.6f, SpriteEffects.None, 0f);
+
+            starColor = StarColor with { A = 0 };
+
+            Main.spriteBatch.Draw(starTexture.Value, starPosition, null, starColor, Projectile.rotation * 0.1f, starOrigin, proj.scale * 0.4f, SpriteEffects.None, 0f);
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            var glowPosition = Projectile.Center + Projectile.gfxOffY * Vector2.UnitY - Main.screenPosition;
+            var glowTexture = CascadeAssets.GlowTexture.Value;
+            var glowOrigin = glowTexture.Size() * 0.5f;
+            var glowScale = Projectile.scale * 1.2f;
+
+            Main.spriteBatch.Draw(glowTexture, glowPosition, null, GlowColor, Projectile.rotation, glowOrigin, glowScale, SpriteEffects.None, 0f);
+
+            return true;
+        }
+
+        public override void PostDrawYoyoString(Vector2 mountedCenter)
+        {
+            if (_stringRenderer is null)
+                return;
+
+            var settings = new YoyoStringRendererSettings(
+                proj: Projectile,
+                start: mountedCenter + Projectile.GetOwner()?.gfxOffY * Vector2.UnitY ?? Vector2.Zero,
+                offset: -Main.screenPosition
+            );
+
+            _stringRenderer.Render(Main.spriteBatch, settings);
+        }
     }
 }
