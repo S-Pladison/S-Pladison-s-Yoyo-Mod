64969422ff82aeac6274d1cb7b65e7031eafe920
spladison@gmail.com <spladison@gmail.com>
2024-11-24 16:11:51 +0300

    Добавление кинжала

--- Content/Items/Vanilla.Yoyos/Gradient.cs
+++ Content/Items/Vanilla.Yoyos/Gradient.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
+using SPYoyoMod.Content.Items.Mod.Yoyos;
 using SPYoyoMod.Content.Particles;
 using SPYoyoMod.Core.Graphics;
 using SPYoyoMod.Core.Graphics.Renderers;
 using SPYoyoMod.Core.Hooks;
 using SPYoyoMod.Utils;
+using System;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.GameContent;
 using Terraria.ID;
@@ -16,8 +19,11 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
     [Autoload(Side = ModSide.Client)]
     public sealed class GradientAssets : ILoadable
     {
+        public const string DaggerPath = $"{_yoyoPath}Gradient_Dagger";
         public const string InvisiblePath = $"{_assetPath}Invisible";
 
+        public static Asset<Texture2D> DaggerGlowTexture { get; private set; } = ModContent.Request<Texture2D>($"{_yoyoPath}Gradient_DaggerGlow");
+        public static Asset<Texture2D> StarTexture { get; private set; } = ModContent.Request<Texture2D>($"{_yoyoPath}Gradient_Star");
         public static Asset<Effect> GodraysEffect { get; private set; } = ModContent.Request<Effect>($"{_yoyoPath}GradientEffect_Godrays", AssetRequestMode.ImmediateLoad); //< Immediate здесь обязателен
 
         private const string _assetPath = $"{nameof(SPYoyoMod)}/Assets/";
@@ -25,6 +31,8 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
 
         void ILoadable.Unload()
         {
+            DaggerGlowTexture = null;
+            StarTexture = null;
             GodraysEffect = null;
         }
 
@@ -42,6 +50,12 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
 
         public override void OnHitNPC(Projectile proj, NPC target, NPC.HitInfo hit, int damageDone)
         {
+            if (!Main.rand.NextBool(5))
+                return;
+
+            if (target.life <= 0)
+                return;
+
             var godraysType = ModContent.ProjectileType<GradientGodraysProjectile>();
 
             if (proj.GetOwner().ownedProjectileCounts[godraysType] > 0)
@@ -124,12 +138,25 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
             if (LifeTimeRatio > 0.75f)
                 return;
 
+            if (Projectile.timeLeft % 6 == 0)
+            {
+                var daggerVelocity = Vector2.UnitY * 12;
+
+                var daggerAimOffset = (Target.position - Target.oldPosition);
+                daggerAimOffset.X += MathF.Sign(daggerAimOffset.X) * daggerVelocity.Y * 3; //< 3 из-за proj.extraUpdates
+                daggerAimOffset.X = MathHelper.Clamp(daggerAimOffset.X, -TileUtils.TileSizeInPixels * 3, TileUtils.TileSizeInPixels * 3);
+
+                var daggerPosition = Projectile.Bottom + new Vector2(Main.rand.NextFloat(-TileUtils.TileSizeInPixels * 3.5f, TileUtils.TileSizeInPixels * 3.5f), -TileUtils.TileSizeInPixels * 45) + daggerAimOffset;
+
+                Projectile.NewProjectile(Projectile.GetSource_FromAI(), daggerPosition, daggerVelocity, ModContent.ProjectileType<GradientDaggerProjectile>(), Projectile.damage, Projectile.knockBack, Projectile.owner, Projectile.Top.Y);
+            }
+
             if (Projectile.timeLeft % 4 == 0)
             {
                 var particle = WorldParticleManager.SpawnParticle<LightPointParticle>();
 
                 particle.LifeTime = ModUtils.SecondsToTicks(1.5f);
-                particle.Position = Projectile.Bottom + new Vector2(Main.rand.NextFloat(-TileUtils.TileSizeInPixels * 6, TileUtils.TileSizeInPixels * 6), Main.rand.NextFloat(-TileUtils.TileSizeInPixels * 70, 6));
+                particle.Position = Projectile.Bottom + new Vector2(Main.rand.NextFloat(-TileUtils.TileSizeInPixels * 4, TileUtils.TileSizeInPixels * 4), Main.rand.NextFloat(-TileUtils.TileSizeInPixels * 70, 6));
                 particle.Velocity = Main.rand.NextVector2Circular(0.2f, 0.5f);
                 particle.StartColor = new Color(255, 250, 185);
                 particle.EndColor = new Color(255, 190, 0);
@@ -173,7 +200,7 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
                 return;
 
             var position = Projectile.Bottom + Projectile.gfxOffY * Vector2.UnitY - Main.screenPosition;
-            var lightningStartPosition = position - Vector2.UnitY * TileUtils.TileSizeInPixels * 100;
+            var lightningStartPosition = position - Vector2.UnitY * TileUtils.TileSizeInPixels * 80;
             var lightningEndPosition = position;
 
             GradientAssets.GodraysEffect
@@ -182,7 +209,7 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
                     parameters["Texture0"].SetValue(TextureAssets.MagicPixel.Value);
                     parameters["TransformMatrix"].SetValue(GameMatrices.Transform * GameMatrices.Projection);
                     parameters["Position"].SetValue(Projectile.Bottom);
-                    parameters["Time"].SetValue(Main.GlobalTimeWrappedHourly);
+                    parameters["Time"].SetValue(Main.GlobalTimeWrappedHourly + Projectile.whoAmI * 12.345f);
                     parameters["Opacity"].SetValue(opacity);
                 })
                 .Apply();
@@ -196,6 +223,169 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
 
     public sealed class GradientDaggerProjectile : ModProjectile
     {
-        public override string Texture { get => GradientAssets.InvisiblePath; }
+        public static readonly int InitTimeLeft = ModUtils.SecondsToTicks(2f);
+        public static readonly EasingBuilder OpacityEasing = new(
+            (EasingFunctions.InOutQuad, 0.25f, 0f, 1f),
+            (EasingFunctions.Linear, 0.65f, 1f, 1f),
+            (EasingFunctions.InOutQuad, 0.1f, 1f, 0f)
+        );
+
+        public override string Texture { get => GradientAssets.DaggerPath; }
+        public ref float HeightToBecomeCollidable { get => ref Projectile.ai[0]; }
+        public bool WasCollided { get => Projectile.ai[0] <= 1; }
+        public float LifeTimeRatio { get => 1f - Projectile.timeLeft / (float)InitTimeLeft; }
+
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.MeleeNoSpeed;
+
+            Projectile.width = 6;
+            Projectile.height = 6;
+
+            Projectile.timeLeft = InitTimeLeft;
+            Projectile.friendly = true;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+            Projectile.extraUpdates = 2;
+
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            //Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<GradientStarProjectile>(), 0, 0, Projectile.owner);
+        }
+
+        public override void AI()
+        {
+            if (!Projectile.tileCollide && Projectile.Bottom.Y >= HeightToBecomeCollidable)
+            {
+                Projectile.tileCollide = true;
+            }
+
+            if (WasCollided)
+            {
+                if (Projectile.timeLeft > 15)
+                {
+                    Projectile.localAI[0] = 1f;
+                    Projectile.extraUpdates = 0;
+                    Projectile.timeLeft = 15;
+                }
+
+                // Если столкновение было с NPC
+                if (Projectile.ai[0] <= 0f)
+                {
+                    var target = Main.npc[-(int)Projectile.ai[0]];
+
+                    if (target.active)
+                        Projectile.position += target.position - target.oldPosition;
+                    else
+                        Projectile.Kill();
+                }
+            }
+
+            if (Projectile.timeLeft == 7)
+                Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.Bottom, Vector2.Zero, ModContent.ProjectileType<GradientStarProjectile>(), 0, 0, Projectile.owner);
+
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2 + MathF.Sin(Projectile.localAI[0] * MathHelper.TwoPi) * (Projectile.whoAmI % 2 == 0 ? 1 : -1) * 0.1f;
+            Projectile.localAI[0] = MathHelper.Max(Projectile.localAI[0] - 0.15f, 0f);
+        }
+
+        public override void ModifyDamageHitbox(ref Rectangle hitbox)
+        {
+            hitbox.Inflate(3, 3);
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (WasCollided)
+                return;
+
+            var vector = Vector2.Normalize(Projectile.velocity);
+
+            Projectile.velocity = vector * 0.0001f;
+            Projectile.Center -= vector * 4f;
+            Projectile.ai[0] = -target.whoAmI; //< WasCollided = true
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.velocity = oldVelocity;
+
+            if (WasCollided)
+                return false;
+
+            var vector = Vector2.Normalize(oldVelocity);
+
+            Projectile.velocity = vector * 0.001f;
+            Projectile.Center += vector * Main.rand.NextFloat(5f, 12f);
+            Projectile.ai[0] = 1; //< WasCollided = true
+            Projectile.hide = true;
+
+            return false;
+        }
+
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            behindNPCsAndTiles.Add(index);
+        }
+
+        public override bool PreDraw(ref Color _)
+        {
+            var position = Projectile.Center - Main.screenPosition;
+
+            var glowTexture = GradientAssets.DaggerGlowTexture;
+            var glowOrigin = glowTexture.Size() * 0.5f;
+            var glowColor = new Color(120, 110, 60, 0) * OpacityEasing.Evaluate(LifeTimeRatio) * 0.5f;
+
+            Main.spriteBatch.Draw(glowTexture.Value, position, null, glowColor, Projectile.rotation, glowOrigin, Projectile.scale, SpriteEffects.None, 0);
+
+            var daggerTexture = TextureAssets.Projectile[Type];
+            var daggerOrigin = daggerTexture.Size() * 0.5f;
+
+            // Хотя функция и передает параметр цвета, его значение не является истинным, поэтому определяем сами
+            var daggerColor = Color.Lerp(Lighting.GetColor(Projectile.Center.ToTileCoordinates(), Color.White), Color.White, 0.4f) * OpacityEasing.Evaluate(LifeTimeRatio);
+
+            Main.spriteBatch.Draw(daggerTexture.Value, position, null, daggerColor, Projectile.rotation, daggerOrigin, Projectile.scale, SpriteEffects.None, 0);
+
+            return false;
+        }
+    }
+
+    public sealed class GradientStarProjectile : ModProjectile, IPostDrawPixelatedProjectile
+    {
+        public static readonly int InitTimeLeft = ModUtils.SecondsToTicks(0.2f);
+        public static readonly EasingBuilder ScaleEasing = new(
+            (EasingFunctions.InOutExpo, 0.4f, 0f, 1f),
+            (EasingFunctions.InOutQuad, 0.6f, 1f, 0f)
+        );
+
+        public override string Texture { get => TheStellarThrowAssets.InvisiblePath; }
+        public float LifeTimeRatio { get => 1f - Projectile.timeLeft / (float)InitTimeLeft; }
+
+        public override void SetDefaults()
+        {
+            Projectile.DefaultToVisualEffect();
+
+            Projectile.timeLeft = InitTimeLeft;
+        }
+
+        public override bool ShouldUpdatePosition()
+        {
+            return false;
+        }
+
+        void IPostDrawPixelatedProjectile.PostDrawPixelated(Projectile _)
+        {
+            var position = Projectile.Center + Projectile.gfxOffY * Vector2.UnitY - Main.screenPosition;
+
+            var starTexture = GradientAssets.StarTexture;
+            var starOrigin = starTexture.Size() * 0.5f;
+            var starScale = ScaleEasing.Evaluate(LifeTimeRatio);
+
+            Main.spriteBatch.Draw(starTexture.Value, position, null, Color.Black * 0.5f, Projectile.rotation * 0.05f, starOrigin, starScale * 0.55f, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(starTexture.Value, position, null, new Color(165, 150, 55) with { A = 0 }, Projectile.rotation * 0.1f, starOrigin, starScale * 0.4f, SpriteEffects.None, 0f);
+        }
     }
 }
