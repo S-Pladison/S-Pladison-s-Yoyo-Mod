d79c3268b8a70a9d749e4436f9f704660e482b99
spladison@gmail.com <spladison@gmail.com>
2024-11-27 07:13:40 +0300

    Добавлено описание для перечисления ChestStyle

--- Utils/Enums/ChestStyle.cs
+++ Utils/Enums/ChestStyle.cs
@@ -1,7 +1,11 @@
 ﻿namespace SPYoyoMod.Utils
 {
+    /// <summary>
+    /// Стиль сундука.
+    /// </summary>
     public enum ChestStyle : int
     {
+        /// <summary>Небесный сундук, который можно найти на парящих островах.</summary>
         Skyware = 13
     }
 }
