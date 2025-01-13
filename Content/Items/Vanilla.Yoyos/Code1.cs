30a4f3da621cf89acb1766fffa52c3b22c4c625e
spladison@gmail.com <spladison@gmail.com>
2025-01-14 02:01:39 +0300

    Добавление описания для Code1

--- Content/Items/Vanilla.Yoyos/Code1.cs
+++ Content/Items/Vanilla.Yoyos/Code1.cs
@@ -1,4 +1,8 @@
-﻿using Terraria.ID;
+﻿using SPYoyoMod.Utils;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
@@ -19,7 +23,25 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
 
     public sealed class Code1Item : VanillaYoyoBaseItem
     {
+        public const int CritBonus = 16;
+
         public override int ItemType => ItemID.Code1;
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(CritBonus);
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            base.ModifyTooltips(item, tooltips); //< Не удалять!
+
+            var critLine = tooltips.Find(VanillaTooltipLine.CritChance);
+
+            if (critLine is null)
+                return;
+
+            ItemUtils.ModifyFirstIntegerInLine(critLine, static (crit) =>
+            {
+                return crit; //< TODO: Модифицировать, если рядом есть враги
+            });
+        }
     }
 
     public sealed class Code1Projectile : VanillaYoyoBaseProjectile
