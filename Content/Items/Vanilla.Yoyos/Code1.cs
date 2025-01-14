0f9788f38b9903cbff7aa6be94ceb2afa1e0ba98
spladison@gmail.com <spladison@gmail.com>
2025-01-14 14:14:20 +0300

    Добавлена логика определения того, активен ли бафф йо-йо Code1

--- Content/Items/Vanilla.Yoyos/Code1.cs
+++ Content/Items/Vanilla.Yoyos/Code1.cs
@@ -6,6 +6,7 @@ using SPYoyoMod.Core.Hooks;
 using SPYoyoMod.Utils;
 using System.Collections.Generic;
 using Terraria;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -48,44 +49,86 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
 
             ItemUtils.ModifyFirstIntegerInLine(critLine, static (crit) =>
             {
-                return crit; //< TODO: Модифицировать, если рядом есть враги
+                return crit + (Main.LocalPlayer.GetModPlayer<Code1Player>().CheckBuffState() ? CritBonus : 0);
             });
         }
     }
 
     public sealed class Code1Projectile : VanillaYoyoBaseProjectile, IInitializableProjectile, IEmitLightEntity
     {
-        public static readonly Color GlowColor = new(65, 185, 255);
+        public sealed class StringSegmentDrawer : IDrawYoyoStringSegments
+        {
+            public Texture2D Texture { get; init; } = TextureAssets.FishingLine.Value;
 
+            public void Draw(SpriteBatch spriteBatch, in YoyoStringRendererSettings settings, IReadOnlyList<YoyoStringSegment> segments)
+            {
+                ref float glowMult = ref settings.Projectile.localAI[1];
+
+                var origin = new Vector2(Texture.Width * 0.5f, 0f);
+                var glowColor = GlowColor * EasingFunctions.InOutQuint(glowMult);
+
+                foreach (var segment in segments)
+                {
+                    var rectangle = new Rectangle(0, 0, Texture.Width, (int)segment.Length);
+                    var color = Color.Lerp(Color.Transparent, glowColor, segment.Index / (float)segments.Count);
+
+                    spriteBatch.Draw(Texture, segment.Position + settings.Offset, rectangle, color, segment.Rotation, origin, 1f, SpriteEffects.None, 0f);
+                }
+            }
+        }
+
+        public static readonly Color GlowColor = new(65, 185, 255);
+        public static readonly float SearchNPCsRadius = TileUtils.TileSizeInPixels * 15f;
+
+        private int _initCritChance;
         private YoyoStringRenderer _stringRenderer;
 
-        public override int ProjType => ProjectileID.Code1;
-        public override bool InstancePerEntity => true;
+        public override int ProjType { get => ProjectileID.Code1; }
+        public override bool InstancePerEntity { get => true; }
 
-        void IInitializableProjectile.Initialize(Projectile _)
+        void IInitializableProjectile.Initialize(Projectile proj)
         {
+            _initCritChance = proj.CritChance;
+
+            ref float glowMult = ref proj.localAI[1];
+            glowMult = 0;
+
             if (Main.netMode == NetmodeID.Server)
                 return;
 
-            _stringRenderer = new YoyoStringRenderer(new IDrawYoyoStringSegments.Gradient(
-               ModContent.Request<Texture2D>(Code1Assets.StringPath, AssetRequestMode.ImmediateLoad).Value,
-               (Color.Transparent, true), (GlowColor, true)
-            ));
+            _stringRenderer = new YoyoStringRenderer(new StringSegmentDrawer());
+        }
+
+        public override void AI(Projectile proj)
+        {
+            var isBuffActive = proj.GetOwner().GetModPlayer<Code1Player>().IsBuffActive;
+
+            // Визуал
+            ref float glowMult = ref (proj as Projectile).localAI[1];
+            glowMult = MathHelper.Clamp(glowMult + (isBuffActive ? 0.05f : -0.05f), 0f, 1f);
+
+            // Свойства
+            proj.CritChance = _initCritChance + (isBuffActive ? Code1Item.CritBonus : 0);
         }
 
         void IEmitLightEntity.EmitLight(Entity proj)
         {
-            Lighting.AddLight(proj.Center, GlowColor.ToVector3() * 0.15f);
+            ref float glowMult = ref (proj as Projectile).localAI[1];
+
+            Lighting.AddLight(proj.Center, GlowColor.ToVector3() * 0.15f * glowMult);
         }
 
         public override bool PreDraw(Projectile proj, ref Color lightColor)
         {
+            ref float glowMult = ref proj.localAI[1];
+
             var glowPosition = proj.Center + proj.gfxOffY * Vector2.UnitY - Main.screenPosition;
             var glowTexture = Code1Assets.GlowTexture.Value;
+            var glowColor = GlowColor * EasingFunctions.InOutQuint(glowMult);
             var glowOrigin = glowTexture.Size() * 0.5f;
             var glowScale = proj.scale * 1.2f;
 
-            Main.spriteBatch.Draw(glowTexture, glowPosition, null, GlowColor, proj.rotation, glowOrigin, glowScale, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(glowTexture, glowPosition, null, glowColor, proj.rotation, glowOrigin, glowScale, SpriteEffects.None, 0f);
 
             return true;
         }
@@ -102,6 +145,39 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
             );
 
             _stringRenderer.Render(Main.spriteBatch, settings);
+        }
+    }
+
+    public sealed class Code1Player : ModPlayer
+    {
+        public static readonly float ScanNPCsRadius = TileUtils.TileSizeInPixels * 23f;
+
+        public bool IsBuffActive { get; private set; }
+
+        public override void PreUpdate()
+        {
+            if (Player.ownedProjectileCounts[ProjectileID.Code1] == 0)
+                return;
+
+            IsBuffActive = CheckBuffState();
+        }
+
+        public bool CheckBuffState()
+        {
+            var nearbyNPCs = new List<NPC>();
+
+            foreach (var npc in Main.ActiveNPCs)
+            {
+                if (!npc.CanBeChasedBy(Player, false))
+                    continue;
+
+                if (Vector2.Distance(npc.Center, Player.Center) > ScanNPCsRadius)
+                    continue;
+
+                nearbyNPCs.Add(npc);
+            }
+
+            return nearbyNPCs.Count > 2;
         }
     }
 }
