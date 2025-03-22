d96b7f70231beae6673d6f93408b5844804a5639
spladison@gmail.com <spladison@gmail.com>
2025-03-23 01:27:31 +0300

    Добавлен новый файл ModSets.cs

---
+++ ModSets.cs
@@ -0,0 +1,28 @@
+﻿using SPYoyoMod.Core;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SPYoyoMod
+{
+    [LoadPriority(sbyte.MinValue)]
+    public sealed class ModSets : ILoadable
+    {
+        public sealed class Items
+        {
+            /// <summary>
+            /// Отвечает за модификацию масштаба отрисовки предмета в слоте инвентаря.
+            /// </summary>
+            public static float?[] InventoryScaleMultiplier { get; internal set; } = Items.InventoryScaleMultiplier = ItemID.Sets.Factory.CreateCustomSet<float?>(null);
+        }
+
+        void ILoadable.Load(Mod mod)
+        {
+            // ...
+        }
+
+        void ILoadable.Unload()
+        {
+            Items.InventoryScaleMultiplier = null;
+        }
+    }
+}
