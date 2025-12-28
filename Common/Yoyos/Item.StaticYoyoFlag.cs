5b76003a24c83c5406bef4232392f56d1f590757
S-Pladison <spladison@gmail.com>
2025-12-28 05:26:25 +0300

    Очередная чистка кода (на этот раз работа над Item.StaticYoyoFlag

--- Common/Yoyos/Item.StaticYoyoFlag.cs
+++ Common/Yoyos/Item.StaticYoyoFlag.cs
@@ -1,28 +1,40 @@
-﻿using SPYoyoMod.Utils;
+﻿using System.Collections.Generic;
+using SPYoyoMod.Utils;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace SPYoyoMod.Common.Yoyos
 {
-    public sealed class StaticYoyoFlagGlobalItem : GlobalItem
+    public sealed class StaticYoyoFlagSystem : ModSystem
     {
-        public override bool AppliesToEntity(Item item, bool lateInstantiation)
-            => false; // Смысла тогда в GlobalItem?.. А я хз, так захотел...
+        private readonly HashSet<int> _yoyosWithoutYoyoFlag = [];
 
         public override void Load()
         {
             // Делаем так, чтобы йо-йо из других модов действительно считались как йо-йо в ванильном понимании...
             ModEvents.OnPostSetupContent += static () =>
             {
+                var instance = ModContent.GetInstance<StaticYoyoFlagSystem>();
+
                 foreach (var (type, item) in ContentSamples.ItemsByType)
                 {
-                    if (item.IsYoyo())
-                    {
-                        ItemID.Sets.Yoyo[type] = true;
-                    }
+                    if (!item.IsYoyo() || ItemID.Sets.Yoyo[type])
+                        continue;
+
+                    instance._yoyosWithoutYoyoFlag.Add(type);
                 }
             };
+        }
+
+        public override void Unload()
+        {
+            foreach (var type in _yoyosWithoutYoyoFlag)
+            {
+                ItemID.Sets.Yoyo[type] = false;
+            }
+
+            _yoyosWithoutYoyoFlag.Clear();
         }
     }
 }
