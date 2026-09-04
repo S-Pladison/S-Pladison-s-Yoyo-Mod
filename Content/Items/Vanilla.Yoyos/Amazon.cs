267d5f1068bb617e7c50fe7b5c5b13c350e1ccdc
S-Pladison <spladison@gmail.com>
2026-09-05 01:08:44 +0300

    Как же мне лень делать этот йо-йо...

--- Content/Items/Vanilla.Yoyos/Amazon.cs
+++ Content/Items/Vanilla.Yoyos/Amazon.cs
@@ -1,5 +1,7 @@
 using SPYoyoMod.Common.Yoyos;
+using Terraria;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
 {
@@ -17,5 +19,23 @@ namespace SPYoyoMod.Content.Items.Vanilla.Yoyos
     public sealed class AmazonProjectile : YoyoProjectile<AmazonItem>
     {
         public override int OverrideType => ProjectileID.JungleYoyo;
+    }
+
+    public sealed class AmazonGlobalNPC : GlobalNPC
+    {
+        public override void Load()
+        {
+
+        }
+
+        public override void Unload()
+        {
+            
+        }
+
+        public override bool PreAI(NPC npc)
+        {
+            return true;
+        }
     }
 }
